677adb05e16e351cdb4bc484f7558507bcf6dd2a
githubuiux <uiuxista@gmail.com>
2023-06-08 13:48:59 +0300

    Initial Commit

---
+++ ExtractFunction/ExtractFunctionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExtractFunctionApp.swift
+//  ExtractFunction
+//
+//  Created by Levent on 8.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct ExtractFunctionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
